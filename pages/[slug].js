8d15768651cbcd06b575b1ad20882db2d2057762
Utkarsh Nagar <utkarshnagar@Utkarshs-MacBook-Air.local>
2024-04-19 15:17:25 +0530

    chore: testing

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router"
 import { Client } from "@notionhq/client"
 import { useEffect, useState } from "react"
 
+const NOTION_BLOG_ID = process.env.NEXT_PUBLIC_NOTION_BLOG_ID
 const NOTION_KEY = process.env.NEXT_PUBLIC_NOTION_KEY
 const notionClient = new Client({auth: NOTION_KEY})
 
@@ -12,19 +13,23 @@ export default function BlogContent(){
     const getData = async () => {
       const { slug } = router.query;
       console.log(router.query, " ", slug);
-      const parts = slug?.split("-");
+      const parts = slug?.split("/");
       const id = parts[parts.length - 1];
-      const pageData = await notionClient.pages
-        .retrieve({ page_id: id })
-        .then((result) => result)
-        .catch((err) => console.error(err));
+      console.log(id);
+    const pageData = await notionClient.databases.query({
+      database_id: NOTION_BLOG_ID,
+     
+    });
+
       console.log(pageData);
       setPostData(pageData);
     };
 
     useEffect(()=>{
-        getData()
-    },[])
+        if(router.query.slug){
+          getData();
+        }
+    },[router.query.slug])
     console.log(postData)
     return(
         <main className="min-h-screen">
