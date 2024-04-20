faea7640e669dfb9cb66d270d5512e80a01e12a7
Utkarsh Nagar <utkarshnagar@Utkarshs-MacBook-Air.local>
2024-04-20 23:04:07 +0530

    fix:config spell fix

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Weekend Coder&apos; Chronicles'
+export const SITE_TITLE = "Weekend Coder's Chronicles"
 export const SITE_DESCRIPTION = 'Blog created using Notion as a CMS'
 export const SITE_KEYWORDS = 'blog, notion, cms, notion-blog'
 export const SITE_AUTHOR = 'Utkarsh Nagar'
