e29cd562db66628a4509cb452898c65ebf017645
Utkarsh Nagar <utkarshnagar@Utkarshs-MacBook-Air.local>
2024-04-20 17:14:21 +0530

    feat: navbar updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,21 @@
 import Link from "next/link";
+import { Github } from 'lucide-react';
 
 export default function Navbar(){
-    return <div className="h-10 my-4 flex row space-x-4 w-4/5">
-        <h2 className="hover:cursor-pointer"><Link href="">Utkarsh Nagar</Link></h2>
-        <h2 className="hover:cursor-pointer"><Link href="https://github.com/Utkarshn10/notion-blog">Source</Link></h2>
-    </div>;
+    return (
+      <div className="h-10 my-4 flex flex-row space-x-6 bg-yellow-100 px-4 rounded-xl">
+        <h2 className="hover:cursor-pointer flex items-center">
+          <Link href="" className="text-xl font-semibold">Utkarsh Nagar</Link>
+        </h2>
+        <h2 className="hover:cursor-pointer flex space-x-1 text-lg">
+          <Link href="https://github.com/Utkarshn10/notion-blog" className="flex items-center"> 
+            <Github  className="h-5 w-5 mr-1"  />
+            Source
+          </Link>
+        </h2>
+        <h2 className="hover:cursor-pointer text-lg flex items-center">
+          <Link href="https://sideprojectss.com/Utkarshn10">Projects</Link>
+        </h2>
+      </div>
+    );
 }
